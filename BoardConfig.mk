77a6cf7ceef7162372da8d020a67b3b804cc23d0
Lijuan Gao <lijuang@codeaurora.org>
2019-07-02 15:57:32 +0800

    TP1803: Set Header version for bootimage
    Set Header version to 1 for bootimage.
    
    Change-Id: Idd2c78689f292dbcdfa7f1eb0b91a8d91aac12c6
    Signed-off-by: Lijuan Gao <lijuang@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,11 @@ BOARD_KERNEL_CMDLINE := \
     lpm_levels.sleep_disabled=1 \
     service_locator.enable=1
 
+# Enable dtb in boot image and Set Header version
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
 # Media
 TARGET_USES_ION := true
 
