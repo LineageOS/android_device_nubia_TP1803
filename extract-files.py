34e7b123307133909c1c39181f7b06d6ba16cb30
Arian <arian.kulmer@web.de>
2024-10-26 18:13:07 +0200

    TP1803: Move to python based extract-utils and patchelf blobs
    Since we can patch our blobs using a newer version of patchelf now,
    we can use more readable names for the libs
    
    Change-Id: I62321b71dc2f8a859521373f7131c284ba26f79c

---
+++ extract-files.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_17_2'
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixup_remove_arch_suffix,
+    lib_fixup_remove_proto_version_suffix,
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_clang_rt_ubsan,
+    libs_proto_3_9_1,
+    libs_proto_21_12,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "device/nubia/TP1803",
+    "hardware/qcom-caf/sm8150",
+    "hardware/qcom-caf/wlan",
+    "vendor/qcom/opensource/dataservices",
+    "vendor/qcom/opensource/display",
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    libs_proto_21_12: lib_fixup_remove_proto_version_suffix,
+    (
+        'libprotobuf-cpp-lite',
+        'libprotobuf-cpp-full',
+    ): lib_fixup_vendorcompat,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libgrallocutils',
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    (
+        'vendor/bin/dspservice',
+        'vendor/bin/vppservice',
+        'vendor/lib64/lib-imsrcs-v2.so',
+        'vendor/lib/libOmxVpp.so',
+        'vendor/lib/libvppclient.so',
+    ): blob_fixup()
+        .remove_needed('libhwbinder.so'),
+    'vendor/lib64/hw/camera.qcom.so': blob_fixup()
+        .add_needed('libcomparetf2.so')
+        .replace_needed('libc++.so', 'libc++-v28.so')
+        .replace_needed('libsnsapi.so', 'libsnsapi-v28.so'),
+    (
+        'vendor/lib64/camera/components/com.qti.camx.chiiqutils.so',
+        'vendor/lib64/camera/components/com.qti.node.eisv2.so',
+        'vendor/lib64/camera/components/com.qti.node.eisv3.so',
+        'vendor/lib64/libcamera_nn_stub.so',
+    ): blob_fixup()
+        .replace_needed('libsnsapi.so', 'libsnsapi-v28.so'),
+    (
+        'vendor/lib64/libwvhidl.so',
+        'vendor/lib64/mediadrm/libwvdrmengine.so',
+    ): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'TP1803',
+    'nubia',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
