6e4ea646b5438cf06349fe8b48794a7a37ddb0a8
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-19 13:22:10 -0500

    TP1803: Migrate rootdir to copy-files
    * Kill Android.mk.
    
    Change-Id: I2e82d31ae0b40aca76c96760156c4bcb402087dd

--- device.mk
+++ device.mk
@@ -218,15 +218,17 @@ PRODUCT_PACKAGES += \
     CarrierConfigOverlay
 
 # Rootdir
-PRODUCT_PACKAGES += \
-    fstab.qcom \
-    init.power.rc \
-    init.qcom.rc \
-    init.qcom.sh \
-    init.qcom.usb.rc \
-    init.recovery.qcom.rc \
-    init.target.rc \
-    ueventd.qcom.rc
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+
+$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
+        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
+$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
+        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
 
 # Sensors
 PRODUCT_PACKAGES += \
