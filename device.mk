fbdacdeb282851d6054a5af0f59d6d916215a67d
Nolen Johnson <johnsonnolen@gmail.com>
2025-02-19 18:32:18 -0500

    TP1803: Copy fstab to ramdisk instead of vendor ramdisk
    Change-Id: I41353293ce34e054da2554b399f0e9a8bed91dd0

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # Rootdir
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
