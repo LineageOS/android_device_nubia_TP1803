85b39caeb7612cf3a05feda4a454b9478859d055
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    TP1803: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig vendor/debugfs.config vendor/ext_config/TP1803.config
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/nubia/sm8150
 
 BOARD_KERNEL_CMDLINE := \
